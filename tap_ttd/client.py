69d525fd153de0afa2063ca0d86d42c077e58f2d
wcb1580 <100547447+wcb1580@users.noreply.github.com>
2025-12-21 09:02:06 +1300

    Update client.py

--- tap_ttd/client.py
+++ tap_ttd/client.py
@@ -105,7 +105,7 @@ class ttdStream(RESTStream):
                 payload = {
                 "ReportDateFormat": "International",
                 "ReportDateRange": "LastXDays",
-                "LookbackDays":1,
+                "LookbackDays":30,
                 "ReportFileFormat": "CSV",
                 "ReportFrequency": "Once",
                 "IncludeHeaders": "true",
